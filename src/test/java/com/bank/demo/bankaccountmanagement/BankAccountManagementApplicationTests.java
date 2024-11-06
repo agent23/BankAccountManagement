d09ff7b42b0e1038a94168cf1c66e7467cb895d3
Stephen <stephen.mohale@bmwithub.co.za>
2024-11-06 13:03:20 +0200

    initial commit

---
+++ src/test/java/com/bank/demo/bankaccountmanagement/BankAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.demo.bankaccountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
